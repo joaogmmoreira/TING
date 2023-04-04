6744b06d0ecf118f907d4ffe4d15495b0dcc1fb2
Joao Moreira <jgmoreira1809@gmail.com>
2023-04-04 18:23:22 -0300

    Req 02 OK

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('txt'):
+            return sys.stderr.write("Formato inválido")
+
+        with open(path_file) as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
