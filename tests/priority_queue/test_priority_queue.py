5ee30e8b162ea23c25310c44bf8bafae2e88b8b9
Joao Moreira <jgmoreira1809@gmail.com>
2023-04-04 20:31:35 -0300

    Req 06 OK

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,34 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    mock = [
+        {"qtd_linhas": 7},
+        {"qtd_linhas": 3},
+    ]
+
+    priority_queue = PriorityQueue()
+
+    assert len(priority_queue) == 0
+
+    priority_queue.enqueue(mock[0])
+
+    assert len(priority_queue) == 1
+
+    priority_queue.enqueue(mock[1])
+
+    assert len(priority_queue) == 2
+
+    assert priority_queue.search(1) == mock[0]
+
+    assert priority_queue.dequeue() == mock[1]
+
+    assert len(priority_queue) == 1
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(90)
+
+    assert priority_queue.dequeue() == mock[0]
+
+    assert len(priority_queue) == 0
