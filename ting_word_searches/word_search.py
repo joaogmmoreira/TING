adee35a4e593f3b20c3ea9fd450b829130e06ed5
Joao Moreira <jgmoreira1809@gmail.com>
2023-04-04 22:37:27 -0300

    Req 08 OK

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    l_word = word.lower()
+    info = []
+
+    for file in range(len(instance)):
+        data = instance.search(file)
+        word_qty = []
+        for index, row in enumerate(data["linhas_do_arquivo"]):
+            if l_word in row.lower():
+                word_qty.append({"linha": index + 1, "conteudo": row})
+
+        if word_qty:
+            info.append({
+                        "palavra": word,
+                        "arquivo": data["nome_do_arquivo"],
+                        "ocorrencias": word_qty
+                        })
+
+    return info
